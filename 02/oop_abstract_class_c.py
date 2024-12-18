09d188d64e08a1eff682d0c7d26dc9bdea490805
Pawel Cyrta <pawel@cyrta.com>
2024-12-18 15:57:03 +0100

    02 OOP in python

---
+++ 02/oop_abstract_class_c.py
@@ -0,0 +1,62 @@
+"""
+* Assignment: OOP AbstractClass Annotate
+* Complexity: easy
+* Lines of code: 5 lines
+* Time: 3 min
+
+English:
+    1. Modify abstract class `Account`
+    2. Add type annotation to all methods and attributes
+    3. Run doctests - all must succeed
+
+Polish:
+    1. Zmodyfikuj klasę abstrakcyjną `Account`
+    2. Dodaj anotację typów do wszystkich metod i atrybutów
+    3. Uruchom doctesty - wszystkie muszą się powieść
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+    >>> from inspect import isabstract, isclass, ismethod, signature
+
+    >>> assert isclass(Account)
+    >>> assert isabstract(Account)
+    >>> assert hasattr(Account, '__init__')
+    >>> assert hasattr(Account, 'login')
+    >>> assert hasattr(Account, 'logout')
+    >>> assert hasattr(Account.__init__, '__isabstractmethod__')
+    >>> assert hasattr(Account.login, '__isabstractmethod__')
+    >>> assert hasattr(Account.logout, '__isabstractmethod__')
+    >>> assert Account.__init__.__isabstractmethod__ == True
+    >>> assert Account.login.__isabstractmethod__ == True
+    >>> assert Account.logout.__isabstractmethod__ == True
+
+    >>> Account.__annotations__
+    {'firstname': <class 'str'>, 'lastname': <class 'str'>}
+
+    >>> Account.__init__.__annotations__
+    {'firstname': <class 'str'>, 'lastname': <class 'str'>, 'return': None}
+
+    >>> Account.login.__annotations__
+    {'return': None}
+
+    >>> Account.logout.__annotations__
+    {'return': None}
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+# Modify class to add type annotation to all methods and attributes
+class Account(ABC):
+    @abstractmethod
+    def __init__(self, firstname, lastname):
+        ...
+
+    @abstractmethod
+    def login(self):
+        ...
+
+    @abstractmethod
+    def logout(self):
+        ...
---
+++ Makefile
@@ -0,0 +1,20 @@
+
+
+
+
+setup:
+	pip  install -r requiements.txt
+
+
+run:
+	analyse.sh plik1.json
+	analyse.sh plik2.json
+    
+
+run-20241217:
+
+
+
+make.py analyse.py plik2.json
+
+
