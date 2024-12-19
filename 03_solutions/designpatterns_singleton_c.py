a2c26a8eb27c1360f61955596bb1ff22e61702e9
Pawel Cyrta <pawel@cyrta.com>
2024-12-19 16:51:57 +0100

    03 solutions

---
+++ 03_solutions/designpatterns_singleton_c.py
@@ -0,0 +1,47 @@
+"""
+* Assignment: DesignPatterns Creational SingletonQueue
+* Complexity: medium
+* Lines of code: 7 lines
+* Time: 5 min
+
+English:
+    1. Create singleton class `Singleton`
+    2. Use `__new__()` object constructor
+    3. Run doctests - all must succeed
+
+Polish:
+    TODO: Polish translation
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+    >>> from pprint import pprint
+
+    >>> class Queue(Singleton):
+    ...     pass
+
+    >>> result_a = Queue()
+    >>> result_b = Queue()
+
+    >>> result_a is result_b
+    True
+"""
+from typing import Self
+
+#
+# class Singleton:
+#     pass
+
+
+# Solution
+class Singleton:
+    instance: Self
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = object.__new__(cls)
+        obj = cls.instance
+        obj.__init__(*args, **kwargs)
+        return obj
+#
+# class Queue(Singleton):
+#     ...     pass
---
+++ 03_solutions/designpatterns_singleton_d.py
@@ -0,0 +1,43 @@
+"""
+* Assignment: DesignPatterns Creational SingletonLogger
+* Complexity: medium
+* Lines of code: 7 lines
+* Time: 5 min
+
+English:
+    1. Create singleton class `Singleton`
+    2. Use `Metaclass`
+    3. Run doctests - all must succeed
+
+Polish:
+    TODO: Polish translation
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+    >>> from pprint import pprint
+
+    >>> class Logger(metaclass=Singleton):
+    ...     pass
+
+    >>> result_a = Logger()
+    >>> result_b = Logger()
+
+    >>> result_a is result_b
+    True
+"""
+from typing import Self
+
+#
+# class Singleton(type):
+#     pass
+
+
+# Solution
+class Singleton(type):
+    instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.instances:
+            cls.instances[cls] = super().__call__(*args, **kwargs)
+        return cls.instances[cls]
+
